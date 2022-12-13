d10607f33da1120d08926abbe89c1c507d4125d7
Nikita Sorokin <nstttrap.sab@gmail.com>
2022-12-13 16:19:13 +0300

    Оператор выбора switch

--- main.cpp
+++ main.cpp
@@ -4,5 +4,30 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
+    int k;
+
+        system("cls");
+        cout << "Select the desired item (specify number):\n1) Summation;\n2) Subtraction;\n3) Multiplication;\n4) Division;\n0)Exit\n";
+        cin >> k;
+        switch(k) {
+        case 1:
+            system("cls");
+            cout << "In the next update...";
+            break;
+        case 2:
+            system("cls");
+            cout << "In the next update...";
+            break;
+        case 3:
+            system("cls");
+            cout << "In the next update...";
+            break;
+        case 4:
+            system("cls");
+            cout << "In the next update...";
+            break;
+        case 0:
+            break;
+        }
     return 0;
 }
